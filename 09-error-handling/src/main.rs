75c44458c69793eaeed15263ace1438cb5d2983a
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-08-06 07:59:44 +0200

    updated chap09

--- 09-error-handling/src/main.rs
+++ 09-error-handling/src/main.rs
@@ -1,3 +1,12 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("../data/test1.txt");
+
+    match file {
+        Ok(_) => println!("File opened successfully!"),
+        Err(e) => {
+            panic!("Failed to open file: {}", e);
+        }
+    }
 }
