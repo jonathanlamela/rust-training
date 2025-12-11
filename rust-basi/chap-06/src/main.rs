ee792063ea29d0bb5dc1ec19bea4c5af7b70b0e0
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-12-11 21:43:07 +0100

    chap06 change

--- rust-basi/chap-06/src/main.rs
+++ rust-basi/chap-06/src/main.rs
@@ -12,6 +12,12 @@ fn main() {
         "The binary representation of {} is {}",
         number, string_representation
     );
+
+    let reconstructed_number: u32 = binary_to_u32(&binary_representation);
+    println!(
+        "The reconstructed number from binary representation is {}",
+        reconstructed_number
+    );
 }
 
 fn convert_to_binary(n: u32) -> Vec<u8> {
@@ -25,6 +31,16 @@ fn convert_to_binary(n: u32) -> Vec<u8> {
     while num > 0 {
         result.push((num % 2) as u8);
         num /= 2;
+    }
+    result
+}
+
+fn binary_to_u32(bits: &[u8]) -> u32 {
+    let mut result: u32 = 0;
+    for (i, &bit) in bits.iter().enumerate() {
+        if bit == 1 {
+            result += 2u32.pow(i as u32);
+        }
     }
     result
 }
