cd957572c6ae431cb593cdcf12c408a7ec1ab6a3
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-12-12 11:46:59 +0100

    updated chap-07

--- rust-basi/chap-07/src/main.rs
+++ rust-basi/chap-07/src/main.rs
@@ -17,4 +17,24 @@ fn main() {
         Country::Spagna => println!("You're in Spain!"),
         Country::Portogallo => println!("You're in Portugal!"),
     }
+
+    if let Country::Italia = current_country {
+        println!("Benvenuti in Italia!");
+    }
+
+    if let Country::Francia = current_country {
+        println!("Bienvenue en France!");
+    }
+
+    if let Country::Germania = current_country {
+        println!("Willkommen in Deutschland!");
+    }
+
+    if let Country::Spagna = current_country {
+        println!("¡Bienvenido a España!");
+    }
+
+    if let Country::Portogallo = current_country {
+        println!("Bem-vindo a Portugal!");
+    }
 }
