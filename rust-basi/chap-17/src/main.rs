aa9a0a615256904d68b87011959b34ef47ead5fb
La Mela, Jonathan <jlamela@bethel.jw.org>
2025-12-18 21:14:26 +0100

    chap 17 update

--- rust-basi/chap-17/src/main.rs
+++ rust-basi/chap-17/src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 fn main() {
     /*
     let args_receved = std::env::args();
@@ -24,4 +26,16 @@ fn main() {
         .parse()
         .expect("Please type a valid number");
     println!("You typed the number: {}", number);
+
+    let mut file_out = std::fs::File::create("output.txt").expect("Failed to create file");
+
+    for i in 1..=100 {
+        writeln!(file_out, "This is line number {}", i).expect("Failed to write to file");
+    }
+
+    let mut file_in =
+        std::fs::File::open("output.txt").expect("Failed to open the file for reading");
+    let mut contents = String::new();
+    std::io::Read::read_to_string(&mut file_in, &mut contents).expect("Failed to read the file");
+    println!("File contents:\n{}", contents);
 }
